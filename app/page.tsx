2df66f5abbb6727803bf69165eb87b2d0e49d0c1
Alejandro <delgadillo.alde@gmail.com>
2025-07-06 17:31:05 -0600

    remove redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
